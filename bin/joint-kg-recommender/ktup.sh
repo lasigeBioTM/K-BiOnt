fb5d6e3c534d99804b35a2f2fbe74c8de9b49429
dpavot <dfsousa@lasige.di.fc.ul.pt>
2021-12-03 17:32:15 +0000

    dockerfile update

--- bin/joint-kg-recommender/ktup.sh
+++
@@ -1,2 +0,0 @@
-python3 run_knowledgable_recommendation.py -data_path ../../corpora/ -log_path log/ -rec_test_files valid.dat:test.dat -kg_test_files valid.dat:test.dat -model_type jtransup -dataset drug_disease -rec_test_files valid.dat:test.dat -num_preferences 2 -nohas_visualization
-
